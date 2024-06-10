ea3bb0c9d5c177d97405a27b54b1314242a87e1b
Sebas2028sra <124327319+Sebas2028sra@users.noreply.github.com>
2024-06-10 16:24:01 -0600

    Actualizacion de la pagina

--- sistema/gestor/apps.py
+++ sistema/gestor/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProjectmanagementConfig(AppConfig):
+class GestorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ProjectManagement'
+    name = 'gestor'
---
+++ sistema/gestor/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse    
+
+# Create your views here.
+
+
+def inicio(request):
+    return render(request, 'paginas/inicio.html')
