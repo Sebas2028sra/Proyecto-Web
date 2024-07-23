ab8be8f1511802a7179dd3f94acc8f16d7286245
Sebas2028sra <124327319+Sebas2028sra@users.noreply.github.com>
2024-07-22 20:20:56 -0600

    Mejoras en lo visual

--- sistema/gestor/urls.py
+++ sistema/gestor/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views
+from .views import registro, login_view, logout_view, inicio, soporte, proyectos, listado_proyectos, agregar_proyecto, actualizar_proyecto, eliminar_proyecto
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('soporte/', views.soporte, name='soporte'),
-    path('index/', views.proyectos, name='index'),
-    path('listado/', views.listado_proyectos, name='listado_proyectos'),
-    path('agregar/', views.agregar_proyecto, name='agregar_proyecto'),
-    path('actualizar/<int:id>/', views.actualizar_proyecto, name='actualizar_proyecto'),
-    path('eliminar/<int:id>/', views.eliminar_proyecto, name='eliminar_proyecto'),
-    path('registro/', views.registro, name='registro'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('', inicio, name='inicio'),
+    path('soporte/', soporte, name='soporte'),
+    path('index/', proyectos, name='index'),
+    path('listado/', listado_proyectos, name='listado_proyectos'),
+    path('agregar/', agregar_proyecto, name='agregar_proyecto'),
+    path('actualizar/<int:id>/', actualizar_proyecto, name='actualizar_proyecto'),
+    path('eliminar/<int:id>/', eliminar_proyecto, name='eliminar_proyecto'),
+    path('accounts/login/', login_view, name='login'),
+    path('accounts/logout/', logout_view, name='logout'),
+    path('accounts/registro/', registro, name='registro'),
 ]
--- sistema/sistema/settings.py
+++ sistema/sistema/settings.py
@@ -124,5 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'inicio'
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/index/'
+LOGOUT_REDIRECT_URL = '/'
