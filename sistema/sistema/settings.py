f1223cf971ef01941b597bfb5e05b0d50531c5e1
Sebas2028sra <124327319+Sebas2028sra@users.noreply.github.com>
2024-08-12 01:21:40 -0600

    Modificaciones esteticas css e imagenes

--- sistema/sistema/settings.py
+++ sistema/sistema/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +124,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = '/imagenes/'
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/index/'
